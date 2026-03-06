0f0a4943cc3b462ff9d87c63b15751dae8083c7c
Carlos Galindo <carlosgalindo20@gmail.com>
2026-03-06 13:40:45 +0100

    add: crear nuevo proyecto springboot

--- BACKEND/RavenloftCastleAPI/src/main/java/com/gvc/ravenloftcastleapi/RavenloftCastleApiApplication.java
+++ BACKEND/RavenloftCastleAPI/src/main/java/com/gvc/ravenloftcastleapi/RavenloftCastleApiApplication.java
@@ -2,12 +2,19 @@ package com.gvc.ravenloftcastleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(scanBasePackages = "com.gvc.ravenloftcastleapi")
 public class RavenloftCastleApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RavenloftCastleApiApplication.class, args);
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println("Admin:    " + encoder.encode("admin"));
+        System.out.println("Carlos:    " + encoder.encode("carlos"));
+        System.out.println("Valentina: " + encoder.encode("valentina"));
+        System.out.println("Gonzalo:   " + encoder.encode("gonzalo"));
+        SpringApplication.run(RavenloftCastleApiApplication.class, args);
 	}
 
 }
