6e3d9524a3c0ae88bc916bb212127ca7add2effd
Carlos Galindo <carlosgalindo20@gmail.com>
2026-03-06 13:44:25 +0100

    add: configuro .env y edito el pom

--- BACKEND/RavenloftCastleAPI/src/main/java/com/gvc/ravenloftcastleapi/RavenloftCastleApiApplication.java
+++ BACKEND/RavenloftCastleAPI/src/main/java/com/gvc/ravenloftcastleapi/RavenloftCastleApiApplication.java
@@ -2,18 +2,13 @@ package com.gvc.ravenloftcastleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication(scanBasePackages = "com.gvc.ravenloftcastleapi")
 public class RavenloftCastleApiApplication {
 
 	public static void main(String[] args) {
-        PasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println("Admin:    " + encoder.encode("admin"));
-        System.out.println("Carlos:    " + encoder.encode("carlos"));
-        System.out.println("Valentina: " + encoder.encode("valentina"));
-        System.out.println("Gonzalo:   " + encoder.encode("gonzalo"));
+
         SpringApplication.run(RavenloftCastleApiApplication.class, args);
 	}
 
